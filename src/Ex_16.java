7d6e52e6bf4847ad57de21068b660ab7b2a4e2e7
José Froes <josepaulo.froes@gmail.com>
2025-11-24 18:14:32 -0300

    Exercicio 16: Crie um pipeline que filtra, mapeia e reduz em uma soma final.

--- src/Ex_16.java
+++ src/Ex_16.java
@@ -1,15 +1,14 @@
-import java.util.*;
-import java.util.stream.IntStream;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 
 public class Ex_16 {
-//    Exercicio 17: Gere uma lista de números aleatórios e filtre > 50.
+//    Exercicio 16: Crie um pipeline que filtra, mapeia e reduz em uma soma final.
     public static void main(String[] args) {
-        Random random = new Random();
-        IntStream stream = random.ints(20, 1 ,70);
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        List<Integer> numeros = new ArrayList<>(stream.boxed().toList());
-
-        numeros.stream().filter(n -> n > 50).forEach(System.out::println);
-
+        Integer soma = numeros.stream().filter(n -> n % 2 == 0).map(n-> n * 3).reduce(Integer::sum).orElse(0);
+        System.out.println(soma);
     }
 }
